8bf7c3fc0f8f316fba3094079e9f4f2864e7442a
rldnd <gi981226@gmail.com>
2024-12-05 11:49:33 +0900

    ci: github workflow

---
+++ src/widgets/ui/BottomNavigation/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./bottomNavigation.module.scss";
+
+const BottomNavigation: React.FC = () => {
+	return <footer className={styles.wrapper}></footer>;
+};
+
+export default BottomNavigation;
